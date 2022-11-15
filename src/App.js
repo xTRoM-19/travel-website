4b998ed33e6a962d4b1c55eac7581d3a46e56be8
Your Name <you@example.com>
2022-11-15 17:05:01 +0700

    Fix New

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router ,Routes,  Route } from 'react-router-dom';
 import React from 'react';
 import './App.css';
-import Home from './components/Pages/Home';
+import Home from './components/Pages/Home'; 
+import Services from './components/Pages/Services';
+import Product from './components/Pages/Product';
+import SignUp from './components/Pages/SignUp';
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
       <Router>
         <Navbar /> 
         <Routes>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/products' element={<Product/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
         </Routes>
       </Router>
     </>
